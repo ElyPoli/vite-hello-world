f4cc3d1f491bae4f9af57ac7a22b4f8cfd0c239e
ElyPoli <itsos.poli@gmail.com>
2023-08-21 15:38:29 +0200

    added app title

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <script>
-	export default {
+import ApplicationTitle from './components/ApplicationTitle.vue';
 
-	}
+export default {
+  components: {
+    ApplicationTitle,
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="bg-primary vh-100">
+    <main class="container d-flex align-items-center justify-content-center vh-100">
+      <ApplicationTitle></ApplicationTitle>
+    </main>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/ApplicationTitle.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="text-center m-o pt-5 pb-5 text-uppercase text-white">Vite Hello World</h1>
+</template>
